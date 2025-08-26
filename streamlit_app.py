1d2bc54fcb474fbefb43f55e7cf2349c42bb6f98
TIRCIS Spin Off - Lhorie <lhorie.pirnay@unamur.be>
2025-08-26 15:32:13 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,11 +14,12 @@ st.write('''
 image = Image.open('image/picture.jpeg')
 st.image(image, width=150)
 
-st.markdown('## Summary', unsafe_allow_html=True)
+st.markdown("## Summary", unsafe_allow_html=True)
 st.info('''
-- Experienced Educator, Researcher and Administrator with almost twenty years of experience in data-oriented environment and a passion for delivering insights based on predictive modeling. 
-- Strong verbal and written communication skills as demonstrated by extensive participation as invited speaker at `10` conferences as well as publishing 149 research articles.
-- Strong track record in scholarly research with H-index of `32` and total citation of 3200+.
+- **Business Intelligence consultant and researcher**, experienced in turning complex data into actionable insights for organizations.  
+- **Founder of TIRCIS**, a spin-off project that transforms traditional dashboards into **causal KPI networks** to anticipate the impact of managerial decisions.  
+- Strong background in **data analysis, functional design, and digital transformation**, with hands-on expertise in **Power BI, SQL, and Python**.  
+- Experienced speaker and trainer, passionate about sharing knowledge in **data governance, strategy maps, and data-driven decision-making**.  
 ''')
 
 #####################
